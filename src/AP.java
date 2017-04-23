ea4877600870840e67987ca24e03987ef68ffbc9
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2017-04-23 22:07:54 +0200

    Finishes grammar with arguments bug

--- src/AP.java
+++ src/AP.java
@@ -8,8 +8,8 @@ public class AP {
 
 	public static void main(String[] args) {
 
-		String inFile = "MS2/Go1.in";
-		String outFile = "MS2/Go1.out";
+		String inFile = "MS2/Go10.in";
+		String outFile = "MS2/Go10.out";
 
 		if (args.length > 1) {
 			inFile = args[0];
